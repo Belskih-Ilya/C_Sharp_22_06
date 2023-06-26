466cd587efa25b81aa86af47e5d94b6576de7cba
BelskihIlya <benny48rus@gmail.com>
2023-06-26 21:06:25 +0300

    lesson 2 task 2-1

---
+++ Lesson_2/2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿int sc_num = new Random().Next(10, 100);
+
+Console.WriteLine(sc_num);
+
+int a = sc_num/10;
+int b = sc_num%10;
+
+if (a>b) 
+{
+    Console.WriteLine("цифра " + a + " наибольшая в числе");
+} else if (b>a) 
+{
+    Console.WriteLine("цифра " + b + " наибольшая в числе");
+} else 
+{
+    Console.WriteLine("цифры в числе одинаковы (" + a + ")");
+}
