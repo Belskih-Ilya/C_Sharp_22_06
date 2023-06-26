c9f5be54dfab92b38912423ba9f5dc222428e8eb
BelskihIlya <benny48rus@gmail.com>
2023-06-26 06:05:01 +0300

    Classwork 1-5

---
+++ Lesson_1/1-5/Program.cs
@@ -0,0 +1,16 @@
+﻿//написать программу, которая на вход принимает одно число, а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите число");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num < 0) {
+    num = -1 * num;
+}
+
+int i = -1*num;
+
+while (i <= num) {
+    Console.WriteLine("" + i);
+    i += 1;
+}
