d2c221844ba140969411934599b02ee4683abec1
BelskihIlya <benny48rus@gmail.com>
2023-07-01 15:35:46 +0300

    Task 3-1 updated

--- Lesson_3/3-1/Program.cs
+++ Lesson_3/3-1/Program.cs
@@ -6,12 +6,17 @@ int x = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter Y");
 int y = int.Parse(Console.ReadLine());
 
-if (x*y > 0)
+if (x == 0 || y == 0)
 {
-    if (x > 0) Console.WriteLine("Dot is in 1st plane");
-    else Console.WriteLine("Dot is in 3rd plane");
-} else
-{
-    if (x > 0) Console.WriteLine("Dot is in 4th plane");
-    else Console.WriteLine("Dot is in 2nd plane");
+    Console.WriteLine("X & Y souldn't be 0");
+} else {
+        if (x * y > 0)
+    {
+        if (x > 0) Console.WriteLine("Dot is in 1st plane");
+        else Console.WriteLine("Dot is in 3rd plane");
+    } else
+    {
+        if (x > 0) Console.WriteLine("Dot is in 4th plane");
+        else Console.WriteLine("Dot is in 2nd plane");
+    }
 }
