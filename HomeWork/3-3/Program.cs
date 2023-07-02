4cfb4acce6c3dc6f07d8f6f424fdbae8b1526e61
BelskihIlya <benny48rus@gmail.com>
2023-07-02 11:16:13 +0300

    Homework task 3-3

---
+++ HomeWork/3-3/Program.cs
@@ -0,0 +1,15 @@
+﻿// написать программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+void Pows3toNum(int num)
+{
+    Console.WriteLine("Table of pow's_3 up to " + num + ":");
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+    }
+    Console.WriteLine("");
+}
+
+Console.WriteLine("Enter a number");
+int num = int.Parse(Console.ReadLine());
+Pows3toNum(num);
