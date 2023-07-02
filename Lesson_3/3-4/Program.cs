32874c698c6bfb8d7a4df8ec70a5336eed38b561
BelskihIlya <benny48rus@gmail.com>
2023-07-02 10:34:34 +0300

    Task 3-4

---
+++ Lesson_3/3-4/Program.cs
@@ -0,0 +1,16 @@
+﻿// написать программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+
+void PowstoNum (int num)
+{
+    Console.WriteLine("Table of pow's up to " + num + ":");
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write(Math.Pow(i, 2) + " ");
+    }
+    Console.WriteLine ("");
+}
+
+Console.WriteLine("Enter a number");
+int num = int.Parse(Console.ReadLine());
+PowstoNum(num);
