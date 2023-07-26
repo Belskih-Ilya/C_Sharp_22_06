294c101a096406289c0251e38738c5b8c8469a32
BelskihIlya <benny48rus@gmail.com>
2023-07-26 10:34:15 +0300

    Lesson 9 Classwork & Homework

---
+++ Lesson_9/9-1/Program.cs
@@ -0,0 +1,19 @@
+﻿// задать значение N. Написать программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+string PrintUpToNReverse (int n)
+{
+    if (n == 1) return $"{n}";
+    return $"{n}, {PrintUpToNReverse(n-1)}";
+}
+
+string PrintUpToN(int m,int n)
+{
+    if (m == n) return $"{m}";
+    return $"{m}, {PrintUpToN((m + 1), n)}";
+}
+
+
+Console.WriteLine("Enter a number");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(PrintUpToN (1, n));
+Console.WriteLine(PrintUpToNReverse(n));
---
+++ Lesson_9/Homework/9-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// задать значения M и N. Написать программу которая дает сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int SumFromMToN(int m, int n)
+{
+    if (m == n) return m;
+    return m + SumFromMToN(m+1, n);
+}
+
+
+Console.WriteLine("Enter a min number");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a max number");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum of numbers from {m} to {n} = {SumFromMToN(m,n)}");
